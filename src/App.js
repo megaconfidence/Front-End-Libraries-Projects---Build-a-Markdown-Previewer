5e9f0c557b63d68b2a456d3b2b4b56a496447367
Confidence Okoghenun <confidenceboi@gmail.com>
2019-11-15 00:06:13 +0100

    feat: Added support for <code>

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Editor from './components/editor.component';
 import Preview from './components/preview.component';
 import Mode from './components/mode.component';
 import './App.css';
-import './fcc';
+// import './fcc';
 import Marked from './marked';
 
 function App() {
-  const text = `Marked - Markdown Parser
+  const text2 = `Marked - Markdown Parser
   ========================
   
   [Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.
@@ -44,6 +44,53 @@ function App() {
   
   [Marked]: https://github.com/markedjs/marked/
   [Markdown]: http://daringfireball.net/projects/markdown/
+  `;
+
+  const text = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+  
+  ![React Logo w/ Text](https://goo.gl/Umyytc)
   `;
   const [input, setInput] = useState({ value: text });
   const [display, setDisplay] = useState({ type: 'preview' });
